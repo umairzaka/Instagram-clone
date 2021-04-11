7c4daaa295c8376ea38d015c466b9c34d2b4faff
Umair Zaka <82205655+umairzaka@users.noreply.github.com>
2021-04-12 03:00:46 +0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,7 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyBXaw21N29ckyIiBf_AOvupS6qHvOmczVk",
-    authDomain: "instagram-clone-f94ea.firebaseapp.com",
-    databaseURL: "https://instagram-clone-f94ea-default-rtdb.firebaseio.com",
-    projectId: "instagram-clone-f94ea",
-    storageBucket: "instagram-clone-f94ea.appspot.com",
-    messagingSenderId: "420490878104",
-    appId: "1:420490878104:web:690bea8cf89aced21204ed",
-    measurementId: "G-FFMMJPTGNG"
+    // firebase config
   });
 
   const db = firebaseApp.firestore();
